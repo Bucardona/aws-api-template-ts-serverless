93f66e945139d7a31fdc2e9793cec4f86d456197
Sebastian Cardona <bucardona@gmail.com>
2023-07-21 16:22:27 -0500

    Update and rename index.ts to getMethod.ts

---
+++ src/getMethod.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda'
+
+export const handler:APIGatewayProxyHandler = async (event:APIGatewayProxyEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'getMethod',
+      event
+    })
+  }
+}
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-export const getIndex = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'getIndex',
-      event
-    })
-  }
-}
